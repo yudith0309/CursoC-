6c1258fd8ea12874e6589fbbeeea5014f59ee424
Yudith recio <yudith0309>
2024-09-22 10:29:19 -0400

    Monadas en c#

---
+++ ProgramacionFuncional/Monadas.cs
@@ -0,0 +1,42 @@
+﻿namespace ProgramacionFuncional
+{
+    public  class Monadas
+    {
+        public Monadas() { 
+        var discount = (decimal monto) => monto / 12;
+        var cargo = (decimal monto) => monto * 4;
+        decimal monto = 100;
+        decimal total = 
+                monto
+                .Pipe(discount)
+                .Pipe(cargo);
+
+        //Ejemplo con Clases 
+        var lis = (List<string> l )=>l.Select(e=>e.ToUpper()).ToList();
+        var lis1 = (List<string> l1)=>l1.OrderBy(e=>e).ToList();
+
+            var beer = new List<string>()
+        {
+            "Cristal","Mayabe","Bucanero"
+        };
+            var beer1 = beer.PipeClass(lis).PipeClass(lis1);
+       }
+       
+    }
+
+    public static class MethodStatic
+    {
+        //Encadenamiento de funciones 
+        public static TOut Pipe<TIn, TOut>(this TIn v, Func<TIn, TOut> FnOut) where TIn : struct where TOut : struct
+        {
+            return FnOut(v);
+        }
+
+        public static TOut PipeClass<TIn, TOut>(this TIn v, Func<TIn, TOut> FnOut) where TIn : class where TOut : class
+        {
+            return FnOut(v);
+        }
+    }
+
+
+}
